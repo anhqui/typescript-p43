1c892d36a1bd2e80795a5e90210c9be0fc435a46
Qui Nguyen <qui@gmail.com>
2024-06-25 23:06:30 -0400

    first commit

---
+++ src/index.ts
@@ -0,0 +1,51 @@
+//---------
+// array
+//---------
+
+let names: string[] = ['Qui', 'John', 'Mary']
+
+let ages: number[] = [25,28,24]
+
+names.push('David')
+ages.push(56);
+
+//----------------------------
+// type inference with arrays
+//----------------------------
+
+let myAge = 32
+let fruits = ['apples', 'pears', 'bananas', 'mangos']
+
+fruits.push('peaches')
+
+const f = fruits[3]
+
+let things = [1,true, 'hello']
+
+const t = things[0]
+
+//-----------------
+// object literals
+//-----------------
+let user: {firstName: string, age: number, id: number} = {
+    firstName: 'Qui',
+    age: 25,
+    id: 1,
+}
+user.firstName = 'John'
+user.id = 2
+
+
+//------------------------------------
+// type inference with object literals
+//------------------------------------
+
+let person = {
+    name: 'Alex',
+    score: 90
+}
+
+person.name = 'Loan'
+person.score = 60
+
+const score = person.score
